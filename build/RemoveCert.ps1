ef89e000b08a6eb1b7cb36299b096a59ecf3d150
Cynthia Z E MacLeod (C3D) <Cynthia.MacLeod@cloud3d.ltd>
2021-10-26 02:05:03 +0100

    Update build certificate

--- build/RemoveCert.ps1
+++ build/RemoveCert.ps1
@@ -1,11 +1,13 @@
 Param(
-   [string]$pfxpath,
-   [string]$password
+   [string]$thumb
 )
-Add-Type -AssemblyName System.Security
-$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-$cert.Import($pfxpath, $password, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]"PersistKeySet")
-$store = new-object system.security.cryptography.X509Certificates.X509Store -argumentlist "MY", CurrentUser
-$store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]"ReadWrite")
-$store.Remove($cert)
-$store.Close()
+if ($thumb -ne "") {
+	Add-Type -AssemblyName System.Security
+	$store = new-object system.security.cryptography.X509Certificates.X509Store -argumentlist "MY", CurrentUser
+	$store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]"ReadWrite")
+	$certs = $store.Certificates.Find([System.Security.Cryptography.X509Certificates.X509FindType]"FindByThumbprint", $thumb, $false)
+	if ($certs -ne $null) {
+		$store.RemoveRange($certs)
+	}
+	$store.Close()
+}
